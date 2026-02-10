6b86de8ceb5ad6a32d02a6d21788cf15b7d61611
Alexander <wurflalexander@gmail.com>
2026-02-10 13:47:49 +0100

    v0.1.3

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@
 bl_info = {
     "name": "AnimationGraph",
     "author": "TeutonStudios",
-    "version": (0, 1, 2),
+    "version": (0, 1, 3),
     "blender": (5, 0, 0),
     "category": "Animation",
 }
