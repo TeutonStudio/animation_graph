71c7aed0f0a709f933cb649fbb4fa17cbaf22881
Alexander <wurflalexander@gmail.com>
2026-02-11 15:01:12 +0100

    refactored AnimNodeTree

--- Core/node_tree.py
+++ Core/node_tree.py
@@ -5,6 +5,37 @@ import bpy
 def register(): bpy.utils.register_class(AnimNodeTree)
 def unregister(): bpy.utils.unregister_class(AnimNodeTree)
 
+class AnimNodeTree(bpy.types.NodeTree):
+    """AnimGraph node tree."""
+
+    bl_idname = "ANIMGRAPH_Tree"
+    bl_label = "Animation Node Editor"
+    bl_icon = "ARMATURE_DATA"
+    bl_description = "Wird verwendet um eine Amatur Pose abhängig vom Zeitpunkt zu definieren"
+    bl_use_group_interface = True
+
+    dirty: bpy.props.BoolProperty(
+        name="Dirty",
+        description="Internal flag set when the node tree changed and needs re-evaluation",
+        default=False,
+        options={"HIDDEN"},
+    )
+
+    def update(self):
+        # Tree dirty markieren -> depsgraph handler bakes etc.
+        self.dirty = True
+
+        # RigInput Node-Ausgänge aktualisieren (optional)
+        for n in getattr(self, "nodes", []): self.update_node(n)
+        for l in getattr(self, "links", []): self.update_link(l)
+
+    def update_node(self,n: bpy.types.Node): pass
+
+    def update_link(self,l: bpy.types.NodeLink): 
+        if isValidLink(l):
+            try: self.links.remove(l)
+            except RuntimeError: pass
+
 validLinks = {
     "NodeSocketBone":[
         "NodeSocketBone"
@@ -37,55 +68,10 @@ validLinks = {
     "NodeSocketMatrix":[
         "NodeSocketMatrix",
     ],
-
 }
+def isValidLink(l: bpy.types.NodeLink) -> bool:
+    vn = l.from_socket.bl_idname
+    zn = l.to_socket.bl_idname
+    return zn in validLinks[vn] or vn == zn
 
-class AnimNodeTree(bpy.types.NodeTree):
-    """AnimGraph node tree."""
 
-    bl_idname = "ANIMGRAPH_Tree"
-    bl_label = "Animation Node Editor"
-    bl_icon = "ARMATURE_DATA"
-    bl_description = "Wird verwendet um eine Amatur Pose abhängig vom Zeitpunkt zu definieren"
-    bl_use_group_interface = True
-
-    dirty: bpy.props.BoolProperty(
-        name="Dirty",
-        description="Internal flag set when the node tree changed and needs re-evaluation",
-        default=False,
-        options={"HIDDEN"},
-    )
-
-    def update(self):
-        # Tree dirty markieren -> depsgraph handler bakes etc.
-        self.dirty = True
-
-        # RigInput Node-Ausgänge aktualisieren (optional)
-        for n in getattr(self, "nodes", []): self.update_node(n)
-        for l in getattr(self, "links", []): self.update_link(l)
-
-    def update_node(self,n: bpy.types.Node): pass
-
-    def update_link(self,l: bpy.types.NodeLink): 
-        fs = l.from_socket
-        ts = l.to_socket
-        if not fs or not ts: return
-
-        def validLink(str, von = fs,nach = ts):
-            if von.bl_idname == nach.bl_idname: return True
-            return von.bl_idname == str and nach.bl_idname in validLinks[str]
-        valid = (
-            validLink("NodeSocketBone") or
-            validLink("NodeSocketInt") or
-            validLink("NodeSocketFloat") or
-            validLink("NodeSocketVectorXYZ") or
-            validLink("NodeSocketRotation") or
-            validLink("NodeSocketVectorTranslation") or
-            validLink("NodeSocketVector") or
-            validLink("NodeSocketMatrix") or
-            False
-        )
-
-        if not valid:
-            try: self.links.remove(l)
-            except RuntimeError: pass
